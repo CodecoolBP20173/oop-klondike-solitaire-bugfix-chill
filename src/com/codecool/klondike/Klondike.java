880b96781d5404bedeec4a4c714dc90ac2130c44
Lukács Bence <lukacsb@gmail.com>
2018-04-12 10:07:56 +0200

    dropdown menu, with working "Quit" button

--- src/com/codecool/klondike/Klondike.java
+++ src/com/codecool/klondike/Klondike.java
@@ -2,6 +2,9 @@ package com.codecool.klondike;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.control.MenuButton;
+import javafx.scene.control.MenuItem;
+import javafx.scene.layout.HBox;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
@@ -19,6 +22,16 @@ public class Klondike extends Application {
         Card.loadCardImages();
         Game game = new Game();
         game.setTableBackground(new Image("/table/green.png"));
+
+        MenuItem menuItem1 = new MenuItem("Restart");
+        MenuItem menuItem2 = new MenuItem("Undo");
+        MenuItem menuItem3 = new MenuItem("Quit");
+
+        MenuButton menuButton = new MenuButton("Menu", null, menuItem1, menuItem2, menuItem3);
+
+        menuItem3.setOnAction((event) -> System.exit(1));
+
+        game.getChildren().addAll(menuButton);
 
         primaryStage.setTitle("Klondike Solitaire");
         primaryStage.setScene(new Scene(game, WINDOW_WIDTH, WINDOW_HEIGHT));
